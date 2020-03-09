e3e4ad1f2eff76f2d2bbe4bda4aeef3d2f899681
Taras K <tk@wwwest.solutions>
2020-03-09 18:42:59 +0200

    heroku deployment fix

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require("express");
-var app = express();
-var dotenv = require("dotenv");
-var StreamChat = require("stream-chat").StreamChat;
-var cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
+const StreamChat = require("stream-chat").StreamChat;
+const cors = require("cors");
+const next = require("next");
+const path = require("path");
+const url = require("url");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 4000;
+const app = express();
 
 dotenv.config();
 
@@ -13,10 +21,62 @@ var chatClient = new StreamChat(
   process.env.GET_STREAM_SECRET
 );
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log(`Server running on port ${process.env.SERVER_PORT}`);
-});
+if (!dev && cluster.isMaster) {
+  console.log(`Node cluster master ${process.pid} is running`);
 
-app.get("/chat", (req, res, next) => {
-  res.json(chatClient.createToken(req.query.user));
-});
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      `Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+    );
+  });
+} else {
+  const nextApp = next({ dir: ".", dev });
+  const nextHandler = nextApp.getRequestHandler();
+
+  nextApp.prepare().then(() => {
+    const server = express();
+
+    if (!dev) {
+      // Enforce SSL & HSTS in production
+      server.use(function(req, res, next) {
+        var proto = req.headers["x-forwarded-proto"];
+        if (proto === "https") {
+          res.set({
+            "Strict-Transport-Security": "max-age=31557600" // one-year
+          });
+          return next();
+        }
+        res.redirect("https://" + req.headers.host + req.url);
+      });
+    }
+
+    // Static files
+    // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+    server.use(
+      "/static",
+      express.static(path.join(__dirname, "static"), {
+        maxAge: dev ? "0" : "365d"
+      })
+    );
+
+    server.get("/chat", (req, res, next) => {
+      res.json(chatClient.createToken(req.query.user));
+    });
+
+    // Default catch-all renders Next app
+    server.get("*", (req, res) => {
+      const parsedUrl = url.parse(req.url, true);
+      nextHandler(req, res, parsedUrl);
+    });
+
+    server.listen(port, err => {
+      if (err) throw err;
+      console.log(`Listening on http://localhost:${port}`);
+    });
+  });
+}

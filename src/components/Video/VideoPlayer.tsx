7efef555093ad40fd83409ee30e20859d45cdeaa
Taras Kum <tk@wwwwest.solutions>
2020-04-09 20:44:06 +0300

    add video overlay for mobil

--- src/components/Video/VideoPlayer.tsx
+++ src/components/Video/VideoPlayer.tsx
@@ -18,13 +18,18 @@ export default ({
   redirectLink = "/",
   hasNext = false
 }) => {
-  const onPlay = () => {
-    let cookieData = Cookies.get(`video__${videoItem.mediaid}`);
+  const [counter, setCounter] = useState(null);
+  const [isFinished, setFinished] = useState(false);
+  const [isPaused, setPaused] = useState(false);
+  const router = useRouter();
 
+  const onPlay = () => {
+    setPaused(false);
+    let cookieData = Cookies.get(`video__${videoItem.mediaid}`);
     const player = window.jwplayer();
 
     if (!cookieData) {
-      player.seek(0);
+      player.seek(1);
       return;
     }
 
@@ -43,7 +48,6 @@ export default ({
     );
   };
 
-  const [counter, setCounter] = useState(null);
   useEffect(() => {
     if (counter && counter > 0) {
       setTimeout(() => setCounter(counter - 1), 1000);
@@ -52,9 +56,6 @@ export default ({
     }
   }, [counter]);
 
-  const [isFinished, setFinished] = useState(false);
-
-  const router = useRouter();
   const onVideoEnded = () => {
     setFinished(true);
     setCounter(5);
@@ -73,6 +74,15 @@ export default ({
       <div className="row">
         <div className="container">
           <ReactJWPlayer
+            onResume={() => {
+              setPaused(false);
+            }}
+            onPlay={() => {
+              setPaused(false);
+            }}
+            onPause={() => {
+              setPaused(true);
+            }}
             onReady={onPlay}
             onTime={onTime}
             onOneHundredPercent={onVideoEnded}
@@ -87,8 +97,8 @@ export default ({
                 <div className="c-video__icon">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="64px"
-                    height="64px"
+                    width="48px"
+                    height="48px"
                     viewBox="0 0 512.13 512.13"
                   >
                     <path
@@ -110,15 +120,42 @@ export default ({
               </div>
             </div>
           )}
+          {isPaused && isMobile() && (
+            <div
+              className="c-video__overlay"
+              onClick={() => {
+                window.jwplayer().play();
+              }}
+            >
+              <div className="c-video__icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="48px"
+                  height="48px"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M256 0C114.833 0 0 114.844 0 256s114.833 256 256 256 256-114.844 256-256S397.167 0 256 0zm0 490.667C126.604 490.667 21.333 385.396 21.333 256S126.604 21.333 256 21.333 490.667 126.604 490.667 256 385.396 490.667 256 490.667z"
+                  />
+                  <path
+                    fill="currentColor"
+                    d="M357.771 247.031l-149.333-96c-3.271-2.135-7.5-2.25-10.875-.396A10.653 10.653 0 00192 160v192c0 3.906 2.125 7.49 5.563 9.365a10.68 10.68 0 005.104 1.302c2 0 4.021-.563 5.771-1.698l149.333-96c3.042-1.958 4.896-5.344 4.896-8.969s-1.854-7.01-4.896-8.969zm-144.438 85.427V179.542L332.271 256l-118.938 76.458z"
+                  />
+                </svg>
+              </div>
+              <div className="c-video__message">Click to play</div>
+            </div>
+          )}
         </div>
-        <div className="container">
-          <div
-            className="c-video__description"
-            dangerouslySetInnerHTML={{
-              __html: DOMPurify.sanitize(videoItem.description)
-            }}
-          ></div>
-        </div>
+      </div>
+      <div className="container">
+        <div
+          className="c-video__description"
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(videoItem.description)
+          }}
+        ></div>
       </div>
     </div>
   );

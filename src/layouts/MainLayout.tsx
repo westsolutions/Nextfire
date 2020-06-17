def8ee8315deae834a7e9c1d362dadd0e34b9657
uliaka <uliana.gorin95@gmail.com>
2020-06-17 19:18:24 +0300

    update version

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loader from "react-loader";
+import { useRouter } from "next/router";
+import { useAuth } from "reactfire";
+import { SIGN_IN } from "@constants/routes";
 import NavBar from "@components/Navigation/NavBar";
 import { version } from "../../package.json";
 
@@ -6,14 +10,43 @@ interface Props {
   children: JSX.Element;
 }
 
-const MainLayout: React.FC<Props> = ({ children }) => (
-  <div className="layout-main">
-    <NavBar />
-    {children}
-    <footer className="footer-app-version">
-      App version: {version ? version : "n/a"}
-    </footer>
-  </div>
-);
+const MainLayout: React.FC<Props> = ({ children }) => {
+  const auth = useAuth();
+  const router = useRouter();
+  const [isLoaded, setLoading] = useState(true);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((currentUser: any) => {
+      if (currentUser && localStorage.getItem(currentUser.email)) {
+        setLoading(true);
+      } else {
+        router.push(SIGN_IN);
+      }
+    });
+    return () => {
+      setLoading(true);
+    };
+  });
+
+  return (
+    <Loader
+      loaded={isLoaded}
+      width={3}
+      color="#fff"
+      length={1}
+      top={"50%"}
+      left={"50%"}
+      position={"fixed"}
+    >
+      <div className="layout-main">
+        <NavBar />
+        {children}
+        <footer className="footer-app-version">
+          App version: {version ? version : "n/a"}
+        </footer>
+      </div>
+    </Loader>
+  );
+};
 
 export default MainLayout;
